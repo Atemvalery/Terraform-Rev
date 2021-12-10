73e9d0ab3fd928c66afca10b3115396694088523
atemvalery <atemval19@gmail.com>
2021-12-10 04:06:55 -0500

    assignment in process

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Terraform-Rev
+# assignment practice
--- provider.tf
+++ provider.tf
@@ -2,3 +2,10 @@ provider "aws" {
     region = "us-east-1"
     
 }
+terraform {
+  backend "s3" {
+    bucket = "maforlap"
+    key    = "user/document/john/"
+    region = "us-east-1"
+  }
+}
